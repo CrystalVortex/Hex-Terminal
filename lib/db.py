34d8ebcb741ecffa18dfc3cf59552a5e5105fcc8
CrystalVortex <fakequasi@gmail.com>
2022-12-20 13:28:28 +0200

    new stuff

--- lib/db.py
+++ lib/db.py
@@ -3,7 +3,7 @@ import sqlite3
 def connect(name):
     global con
     con = sqlite3.connect(name+".db")
-    print(f"Connected to: {name}.db")
+    print(f"Connected to: {name}.db, execute commands with db.execute(value)")
 
 
 def execute(val):
