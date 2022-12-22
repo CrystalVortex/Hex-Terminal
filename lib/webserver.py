17e1ecaef8c5b148e9af70d655158fea5aba95e1
CrystalVortex <fakequasi@gmail.com>
2022-12-22 21:13:38 +0200

    Dont know why this file is here

--- lib/webserver.py
+++
@@ -1,9 +0,0 @@
-import os
-from http.server import HTTPServer, CGIHTTPRequestHandler
-
-def start():
-    os.chdir('.')
-    print("Warning: Starting the web server will not allow you to exit until you do ctrl + C")
-    server_object = HTTPServer(server_address=('', 80), RequestHandlerClass=CGIHTTPRequestHandler)
-    print("Started on port 80... (localhost)")
-    server_object.serve_forever()
